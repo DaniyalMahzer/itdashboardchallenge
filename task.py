de2e94ac16ccfa3bfcabf11ea2ce31325be87df4
Daniyal <daniyal.mazher@gmail.com>
2021-09-06 15:55:17 +0500

    update

--- task.py
+++ task.py
@@ -1,6 +1,7 @@
 from time import sleep
 import os
 from RPA.Browser.Selenium import Selenium
+from RPA.Excel.Application import Application
 
 
 class ItDashboard:
@@ -8,6 +9,7 @@ class ItDashboard:
 
     def __init__(self, url):
         self.browser = Selenium()
+        self.app = Application()
         self.browser.open_available_browser(url)
         self.browser.wait_until_page_contains_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a')
         self.browser.find_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a').click()
@@ -15,11 +17,17 @@ class ItDashboard:
             ('//div[@id="agency-tiles-widget"]//div[@class="col-sm-4 text-center noUnderline"]')
         self.browser.set_download_directory(os.path.join(os.getcwd(), "output/"))
         sleep(10)
-        print(self.agencies)
-        for item in self.agencies:
-            data = item.text.split("\n")
-            print(data[0], data[2])
 
+    def agencies_excel(self):
+        self.app.open_application()
+        self.app.open_workbook('Agencies.xlsx')
+        for i, item in enumerate(self.agencies):
+                agency_data = item.text.split("\n")
+                self.app.write_to_cells(row=i, column=i, value=agency_data[0])
+                self.app.write_to_cells(row=i, column=i+1, value=agency_data[2])
+        self.app.save_excel()
+        self.app.quit_application()
 
 if __name__ == "__main__":
     it_dashboard = ItDashboard("https://itdashboard.gov/")
+    it_dashboard.agencies_excel()
