7320bcdc71c20eaa1eed9635092fb616c1b49bdb
Daniyal <daniyal.mazher@gmail.com>
2021-09-06 13:45:08 +0500

    Task update

--- task.py
+++ task.py
@@ -1,9 +1,18 @@
-"""Template robot with Python."""
+from time import sleep
+
+from RPA.Browser.Selenium import Selenium
 
 
-def minimal_task():
-    print("Done.")
+class ItDashboard:
+
+    def __init__(self, url):
+        self.browser = Selenium()
+        self.browser.open_available_browser(url)
+        self.browser.wait_until_page_does_not_contain_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a')
+        self.browser.find_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a').click()
+        sleep(10)
 
 
 if __name__ == "__main__":
-    minimal_task()
+    it_dashboard = ItDashboard()
+
