c964170a533f34ff6dca9bb56db6dcab830867b1
Daniyal <daniyal.mazher@gmail.com>
2021-09-06 14:48:43 +0500

    Task update

--- task.py
+++ task.py
@@ -1,19 +1,23 @@
 from time import sleep
-
+import os
 from RPA.Browser.Selenium import Selenium
 
 
 class ItDashboard:
+    agencies = []
 
     def __init__(self, url):
         self.browser = Selenium()
         self.browser.open_available_browser(url)
         self.browser.wait_until_page_contains_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a')
         self.browser.find_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a').click()
+        self.agencies = self.browser.find_elements('//*[@id="agency-tiles-widget"]')
+        self.browser.set_download_directory(os.path.join(os.getcwd(), "output/"))
         sleep(10)
-        
 
+    def test_print(self):
+        print(self.agencies)
 
 if __name__ == "__main__":
     it_dashboard = ItDashboard("https://itdashboard.gov/")
-
+    it_dashboard.test_print()
