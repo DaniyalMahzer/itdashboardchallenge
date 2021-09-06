251a6006864f8dc20f1cca200145204f8953d740
Daniyal <daniyal.mazher@gmail.com>
2021-09-06 17:43:27 +0500

    update

--- task.py
+++ task.py
@@ -1,34 +1,39 @@
-from time import sleep
 import os
 from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
 
 
 class ItDashboard:
-    agencies_data = {}
-    def __init__(self, url):
+    agencies = []
+
+    def __init__(self):
         self.browser = Selenium()
         self.files = Files()
-        self.browser.open_available_browser(url)
+        self.browser.open_available_browser("https://itdashboard.gov/")
+        self.browser.set_download_directory(os.path.join(os.getcwd(), "output/"))
+
+    def get_agencies(self):
         self.browser.wait_until_page_contains_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a')
         self.browser.find_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a').click()
         self.agencies = self.browser.find_elements\
             ('//div[@id="agency-tiles-widget"]//div[@class="col-sm-4 text-center noUnderline"]')
-        self.browser.set_download_directory(os.path.join(os.getcwd(), "output/"))
-        sleep(10)
 
-    def get_agencies(self):
-        agencies = []
+    def write_agencies(self):
+        companies = []
         investments = []
         for item in self.agencies:
             agency_data = item.text.split('\n')
-            agencies.append(agency_data[0])
+            companies.append(agency_data[0])
             investments.append(agency_data[2])
-        entries = {"companies": agencies, "investmants": investments}
+        entries = {"companies": companies, "investments": investments}
         wb = self.files.create_workbook("output/Agencies.xlsx")
         wb.append_worksheet("Sheet", entries)
         wb.save()
 
-if __name__ == "__main__":
-    it_dashboard = ItDashboard("https://itdashboard.gov/")
-    it_dashboard.get_agencies()
+    def make_agency_excel(self):
+        self.get_agencies()
+        self.write_agencies()
+
+
+obj = ItDashboard()
+obj.make_agency_excel()
