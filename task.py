461017631143e869ab302d91d06d8966c4a7ff0d
DaniyalMahzer <85176106+DaniyalMahzer@users.noreply.github.com>
2021-09-06 13:53:03 +0500

    update

--- task.py
+++ task.py
@@ -8,7 +8,7 @@ class ItDashboard:
     def __init__(self, url):
         self.browser = Selenium()
         self.browser.open_available_browser(url)
-        self.browser.wait_until_page_does_not_contain_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a')
+        self.browser.wait_until_page_contains_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a')
         sleep(10)
         self.browser.find_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a').click()
         
