e8f777a68eecb28019dd72c7347909ed08090de5
Daniyal <daniyal.mazher@gmail.com>
2021-09-06 16:06:33 +0500

    update

--- task.py
+++ task.py
@@ -19,15 +19,23 @@ class ItDashboard:
         sleep(10)
 
     def agencies_excel(self):
-        self.app.open_application()
-        self.app.open_workbook('Agencies.xlsx')
-        self.app.set_active_worksheet(sheetname='Agencies')
-        for i, item in enumerate(self.agencies):
-                agency_data = item.text.split("\n")
-                self.app.write_to_cells(row=i, column=i, value=agency_data[0])
-                self.app.write_to_cells(row=i, column=i+1, value=agency_data[2])
-        self.app.save_excel()
-        self.app.quit_application()
+        app = Application()
+
+        app.open_application()
+        app.open_workbook('workbook.xlsx')
+        app.set_active_worksheet(sheetname='new stuff')
+        app.write_to_cells(row=1, column=1, value='new data')
+        app.save_excel()
+        app.quit_application()
+        # self.app.open_application()
+        # self.app.open_workbook('Agencies.xlsx')
+        # self.app.set_active_worksheet(sheetname='Agencies')
+        # for i, item in enumerate(self.agencies):
+        #         agency_data = item.text.split("\n")
+        #         self.app.write_to_cells(row=i, column=i, value=agency_data[0])
+        #         self.app.write_to_cells(row=i, column=i+1, value=agency_data[2])
+        # self.app.save_excel()
+        # self.app.quit_application()
 
 
 if __name__ == "__main__":
