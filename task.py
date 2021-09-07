85cc9383d2b532cbc6153f1722f97a14c5592e0b
Daniyal <daniyal.mazher@gmail.com>
2021-09-07 18:34:33 +0500

    update

--- task.py
+++ task.py
@@ -12,6 +12,7 @@ class ItDashboard:
         self.browser = Selenium()
         self.files = Files()
         self.browser.open_available_browser("https://itdashboard.gov/")
+        self.browser.set_download_directory(os.path.join(os.getcwd(), "output/"))
 
     def get_agencies(self):
         self.browser.wait_until_page_contains_element('//*[@id="node-23"]/div/div/div/div/div/div/div/a')
@@ -48,16 +49,15 @@ class ItDashboard:
     def scrap_agency(self, agency_open):
         agency = self.agencies[agency_open]
         self.browser.wait_until_page_contains_element(agency)
-        self.browser.find_element(agency).click()
+        url = self.browser.find_element(agency).find_element_by_tag_name("a").get_attribute("href")
+        self.browser.go_to(url)
         self.browser.wait_until_page_contains_element('//*[@id="investments-table-object_info"]',
                                                       timeout=timedelta(seconds=50))
         raw_total = self.browser.find_element('//*[@id="investments-table-object_info"]')
         data = raw_total.text.split(" ")
         total_entries = int(data[-2])
+        self.browser.wait_until_page_contains_element('//*[@id="investments-table-object_length"]/label/select')
         self.browser.find_element('//*[@id="investments-table-object_length"]/label/select').click()
-        self.browser.find_element('//*[@id="investments-table-object_length"]/label/select/option[4]').click()
-        self.browser.wait_until_page_contains_element(
-            f'//*[@id="investments-table-object"]/tbody/tr[{total_entries}]/td[1]', timeout=timedelta(seconds=30))
         self.get_headers()
         uii_ids = [self.headers[0]]
         bureau = [self.headers[1]]
@@ -68,6 +68,7 @@ class ItDashboard:
         num_of_project =[self.headers[6]]
         for i in range(1, total_entries + 1):
             item = self.browser.find_element(f'//*[@id="investments-table-object"]/tbody/tr[{i}]/td[1]')
+            uii_ids.append(item.text)
             try:
                 bureau_current = self.browser.find_element(f'//*[@id="investments-table-object"]/tbody/tr[{i}]/td[2]').text
                 investment_title_current = self.browser.find_element(f'//*[@id="investments-table-object"]/tbody/tr[{i}]/td[3]').text
@@ -89,30 +90,32 @@ class ItDashboard:
             except:
                 link = ''
             if link:
-                downloader = Selenium()
-                downloader.open_available_browser(link)
-                downloader.find_element('//div[@id="business-case-pdf"]').click()
-                downloader.set_download_directory(os.path.join(os.getcwd(), "output/"))
+                self.browser.go_to(link)
+                self.browser.wait_until_page_contains_element('//div[@id="business-case-pdf"]')
+                self.browser.find_element('//div[@id="business-case-pdf"]').click()
                 while True:
                     try:
                         sleep(2)
-                        if downloader.find_element('//div[@id="business-case-pdf"]').find_element_by_tag_name("span"):
-                            sleep(1)
+                        if self.browser.find_element('//div[@id="business-case-pdf"]').find_element_by_tag_name("span"):
+                            sleep(2)
                         else:
                             break
                     except:
-                        if downloader.find_element('//*[contains(@id,"business-case-pdf")]//a[@aria-busy="false"]'):
-                            sleep(1)
+                        if self.browser.find_element('//*[contains(@id,"business-case-pdf")]//a[@aria-busy="false"]'):
+                            sleep(2)
                             break
                 sleep(2)
-                downloader.close_browser()
+                self.browser.go_to(url)
+                self.browser.wait_until_page_contains_element('//*[@id="investments-table-object_length"]/label/select', timeout=timedelta(seconds=20))
+                self.browser.wait_until_page_contains_element('//*[@id="investments-table-object_length"]/label/select')
+                self.browser.find_element('//*[@id="investments-table-object_length"]/label/select').click()
+                sleep(10)
             bureau.append(bureau_current)
             investment_title.append(investment_title_current)
             total_FY2021.append(total_FY2021_current)
             type_agency.append(type_agency_current)
             CIO_rating.append(CIO_rating_current)
             num_of_project.append(num_of_project_current)
-            uii_ids.append(item.text)
         data = {"uii": uii_ids,
                 "bureau": bureau,
                 "company": investment_title,
@@ -121,7 +124,7 @@ class ItDashboard:
                 "CIO rating": CIO_rating,
                 "# of project": num_of_project,
                 }
-        wb = self.files.create_workbook("output/uii.xlsx")
+        wb = self.files.create_workbook("output/uii table.xlsx")
         wb.append_worksheet("Sheet", data)
         wb.save()
 
